2dcb8408835597c6b2c3905ff3a5adffc1ffe411
smml1996 <stefanie.muroya@ucsp.edu.pe>
2025-08-27 16:45:40 +0200

    quantum gates

---
+++ include/quantum_gates.hpp
@@ -0,0 +1,60 @@
+#include <unordered_set>
+
+using namespace std;
+
+enum GateName {
+    // this enum just helps to keep track of the basis gates of the quantum hardware
+    // one-qubit ops
+    I,
+    X,
+    Y,
+    Z,
+    H,
+    S,
+    Sd,
+    Sx,
+    Sxd,
+    U1,
+    U2,
+    U3,
+    T,
+    Td,
+    Rz,
+    Ry,
+    Rx,
+    Reset,
+    Meas,
+    Custom,
+
+    // MULTI-QUBIT GATES
+    Cnot,
+    Ecr,
+    Rzx,
+    Cz,
+    Ch,
+    Swap,
+    Toffoli,
+
+    // classical gates
+    Write0,
+    Write1,
+
+    count
+};
+
+enum BasisGates {
+    TYPE1,
+    TYPE2,
+    TYPE3,
+    TYPE4,
+    TYPE5,
+    TYPE6,
+
+    count
+};
+
+unordered_set<GateName> get_value(const BasisGates& basis_gates);
+
+BasisGates get_basis_gates_type(const unordered_set<GateName> &basis_gates);
+
+GateName get_enum_obj(const string &raw_gate);
---
+++ src/quantum_gates.cpp
@@ -0,0 +1,103 @@
+#include "../include/quantum_gates.hpp"
+#include <cassert>
+
+using namespace std;
+
+unordered_set<GateName> get_value(const BasisGates& basis_gates) {
+    unordered_set<GateName> result;
+
+    switch (basis_gates) {
+        TYPE1:
+            return unordered_set<GateName>({
+                GateName::Cnot, GateName::Meas, GateName::Reset, GateName::Rz, GateName::Sx, GateName::X
+            });
+            break;
+        TYPE2:
+            return unordered_set({
+                GateName::Cz, GateName::Meas, GateName::Reset, GateName::Rz, GateName::Sx, GateName::X
+            });
+            break;
+        TYPE3:
+            return unordered_set({
+                GateName::U1, GateName::Reset, GateName::U3, GateName::Meas, GateName::U2, GateName::Cnot
+            });
+            break;
+        TYPE4:
+            return unordered_set({
+                GateName::Reset, GateName::Meas, GateName::Rz, GateName::Sx, GateName::X
+            });
+            break;
+        TYPE5:
+            return unordered_set({
+                GateName::Rz, GateName::Meas, GateName::Reset, GateName::Sx, GateName::Ecr, GateName::X
+            });
+            break;
+        TYPE6:
+            return unordered_set({
+                GateName::Rz, GateName::Meas, GateName::Reset, GateName::Sx, GateName::Ecr, GateName::X, GateName::Cnot
+            });
+            break;
+        default:
+            assert(false);
+    }
+}
+
+BasisGates get_basis_gates_type(const unordered_set<GateName> &basis_gates) {
+    for(int i = 0; i < BasisGates::count; i++) {
+        BasisGates current_bg = static_cast<BasisGates>(i);
+        auto val = get_value(current_bg);
+        if (val == basis_gates) {
+            return current_bg;
+        }
+    }
+
+    throw std::invalid_argument("Unknown basis_gates set");
+}
+
+inline std::string to_string(GateName gate) {
+    switch (gate) {
+        case I:        return "I";
+        case X:        return "X";
+        case Y:        return "Y";
+        case Z:        return "Z";
+        case H:        return "H";
+        case S:        return "S";
+        case Sd:       return "SD";
+        case Sx:       return "SX";
+        case Sxd:      return "SXD";
+        case U1:       return "U1";
+        case U2:       return "U2";
+        case U3:       return "U3";
+        case T:        return "T";
+        case Td:       return "TD";
+        case Rz:       return "RZ";
+        case Ry:       return "RY";
+        case Rx:       return "RX";
+        case Reset:    return "RESET";
+        case Meas:     return "MEAS";
+        case Custom:   return "CUSTOM";
+
+        case Cnot:     return "CNOT";
+        case Ecr:      return "ECR";
+        case Rzx:      return "RZX";
+        case Cz:       return "CZ";
+        case Ch:       return "CH";
+        case Swap:     return "SWAP";
+        case Toffoli:  return "TOFFOLI";
+
+        case Write0:   return "WRITE0";
+        case Write1:   return "WRITE1";
+    }
+
+    throw std::invalid_argument("Unknown GateName enum value");
+}
+
+GateName get_enum_obj(const string &raw_gate) {
+    for (int i = 0; i < GateName::count; i++) {
+        GateName g = static_cast<GateName>(i);
+        if(to_string(g) == raw_gate) {
+            return g;
+        }
+    }
+    throw std::invalid_argument("Invalid raw gate name " + raw_gate);
+}
