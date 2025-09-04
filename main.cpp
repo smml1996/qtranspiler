1fa0a4cf09271ea3d5f042c8c776739f3dbbdf58
smml1996 <stefanie.muroya@ucsp.edu.pe>
2025-09-05 01:12:09 +0200

    adds cxxopts

--- main.cpp
+++ main.cpp
@@ -1,20 +1,6 @@
-#include "antlr4-runtime.h"
-#include "grammars/assertions/AssertionLexer.h"
-#include "grammars/assertions/AssertionParser.h"
+#include "cxxopts.hpp"
 
-using namespace antlr4;
 
 int main() {
-    std::string inputText = "assert q0 = 1 and (q1 != 0 or q2 = 5)";
-
-    ANTLRInputStream input(inputText);
-    AssertionLexer lexer(&input);
-    CommonTokenStream tokens(&lexer);
-    AssertionParser parser(&tokens);
-
-    tree::ParseTree *tree = parser.assertion();  // parse entry point
-
-    std::cout << "Parse tree: " << tree->toStringTree(&parser) << std::endl;
-
     return 0;
 }
