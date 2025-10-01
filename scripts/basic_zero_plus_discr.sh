ad5cb8202d24c69b6d42d103598254d462dce693
smml1996 <stefanie.muroya@ucsp.edu.pe>
2025-10-01 10:51:41 +0200

    scripts

---
+++ scripts/basic_zero_plus_discr.sh
@@ -0,0 +1,5 @@
+sbatch server_script.sh basic_zero_plus_discr basic_zero_plus_discr_0 convex yorktown,bogota,ourense,lagos,jakarta,boeblingen,hanoi,toronto,cambridge,cusco,washington 3 3
+sbatch server_script.sh basic_zero_plus_discr basic_zero_plus_discr_1 convex athens,santiago,essex,perth,perfect_hardware,singapore,auckland,sydney,prague,brisbane,kyiv 3 3
+sbatch server_script.sh basic_zero_plus_discr basic_zero_plus_discr_2 convex vigo,rome,manila,casablanca,melbourne,Johannesburg,montreal,cairo,rochester,kawasaki,torino 3 3
+sbatch server_script.sh basic_zero_plus_discr basic_zero_plus_discr_3 convex belem,burlington,london,oslo,guadalupe,poughkeepsie,mumbai,paris,brooklyn,osaka,fez 3 3
+sbatch server_script.sh basic_zero_plus_discr basic_zero_plus_discr_4 convex valencia,quito,lima,nairobi,almaden,kolkata,geneva,algiers,manhattan,kyoto,makarresh 3 3
